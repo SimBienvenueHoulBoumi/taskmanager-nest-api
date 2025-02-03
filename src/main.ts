f3301b51a02f6d2b84638057a6c214c5a2bd3562
SimBienvenueHoulBoumi <brhulla@gmail.com>
2025-02-03 21:08:19 +0100

    clean

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  // Configuration de Swagger
   const config = new DocumentBuilder()
     .setTitle('Tasks API')
     .setDescription('The tasks API description')
     .setVersion('1.0')
     .build();
+
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
-
-// http://localhost:3000/api
